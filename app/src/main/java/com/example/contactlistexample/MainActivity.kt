29b976ec76038028fc4007584726e2184b185624
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-11-29 22:42:50 -0300

    add Textview and validations to add button

--- app/src/main/java/com/example/contactlistexample/MainActivity.kt
+++ app/src/main/java/com/example/contactlistexample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.contactlistexample
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactlistexample.adapter.ContactAdapter
 import com.example.contactlistexample.data.Contact
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,8 +25,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Form elements find
-       // val etName = findViewById<EditText>(R.id.etName)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etPhone = findViewById<EditText>(R.id.etPhone)
+        val cbAvailable = findViewById<CheckBox>(R.id.cbAvailable)
 
+        val textView = findViewById<TextView>(R.id.textView)
+        val btnAdd = findViewById<Button>(R.id.addButton)
+
+        // Set on click listener for add button
+        btnAdd.setOnClickListener {
+            val name = etName.text.toString()
+            val phone = etPhone.text.toString()
+            val isAvailable = cbAvailable.isChecked
+
+            // Validations
+            if(name.isNotEmpty() || phone.isNotEmpty()){
+                textView.text = "Welcome $name"
+                textView.visibility = TextView.VISIBLE
+            } else if (name.isNotEmpty() || phone.isNotEmpty() || !isAvailable) {
+                textView.text = "All fields are required"
+                textView.visibility = TextView.VISIBLE
+            } else {
+                textView.visibility = TextView.INVISIBLE
+            }
+        }
     }
 
     // RecyclerView
